d53c019a9a7d8d0262b9327a18bccab13947bd0f
haroldsikos123 <jhonharoldsikosastete@gmail.com>
2026-01-28 11:09:12 -0500

    a

--- InventorySystem_Project/src/main/java/com/inventorysystem_project/controllers/ProveedorController.java
+++ InventorySystem_Project/src/main/java/com/inventorysystem_project/controllers/ProveedorController.java
@@ -18,6 +18,12 @@ public class ProveedorController {
     @Autowired
     private IProveedorService proveedorService;
 
+    @GetMapping("/paises")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER') or hasAuthority('GUEST')")
+    public List<ProveedorDTO> listarPaises() {
+        return List.of();
+    }
+
     @PostMapping("/registrar")
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER') or hasAuthority('GUEST')")
     public void registrar(@RequestBody ProveedorDTO dto) {
